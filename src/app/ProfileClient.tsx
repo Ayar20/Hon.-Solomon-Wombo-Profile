fbdaa73c773088acb7e4686e3aaa671468764f4c
Ayar20 <93492608+Ayar20@users.noreply.github.com>
2026-04-06 15:03:43 +0100

    Fix emblems: assign National Assembly and House of Rep images to respective slots

--- src/app/ProfileClient.tsx
+++ src/app/ProfileClient.tsx
@@ -63,7 +63,7 @@ export default function ProfileClient({ initialData }: { initialData: any }) {
         <div className="container">
           <div className="emblems-grid">
             <div className="emblem-item">
-              <img src="/House of Rep.png" alt="Nigerian Coat of Arms" />
+              <img src="/National Assembly.png" alt="Nigerian Coat of Arms" />
               <span>Federal Republic<br />of Nigeria</span>
             </div>
             <div className="emblem-item">
